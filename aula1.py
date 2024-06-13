b5891b284ef7670208186bed819023fffbb5cf09
mgeambastiane <mgeambastiane@gmail.com>
2024-06-13 20:55:03 -0300

    Adicionando os tipos de format

--- aula1.py
+++ aula1.py
@@ -3,10 +3,16 @@ O aluno Matheus teve a primeira nota 2, a segunda nota 3, e a nota final 5
 
 """
 
-nome = "Matheus"
-n1 = 2
-n2 = 32
-soma = n1 + n2
+nome = "Paula Martins"
+mes = "Janeiro"
+valor_compra = 5
+desconto = 10
+cupom = "PAULAÉ10"
 
 
-print("O aluno " + nome + " teve a primeira nota " , n1 , " a segunda nota" , n2 , "e a nota final " , soma  )
+mensagem = f"O nome da pessoa é {nome}, desconto dela é {desconto}"
+
+print(mensagem)
+
+
+
