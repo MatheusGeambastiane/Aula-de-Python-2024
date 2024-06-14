a34c45f0f27f319b008b4f707b69186cb5f97c15
mgeambastiane <mgeambastiane@gmail.com>
2024-06-13 21:04:20 -0300

    criando arquivo de type

---
+++ type.py
@@ -0,0 +1,15 @@
+
+
+nome = "Paula Martins"
+mes = "Janeiro"
+valor_compra = 5
+desconto = 10
+cupom = "PAULAÉ10"
+
+
+tipo_nome = type(nome)
+tipo_desconto = type(desconto)
+
+
+print(tipo_nome)
+print(tipo_desconto)
