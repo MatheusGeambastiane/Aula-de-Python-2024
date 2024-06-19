a2948ad748547b34aa54bcad2e386e2c04dd5dce
MatheusGeambastiane <mgeambastiane@gmail.com>
2024-06-18 21:47:39 -0300

    Aula 3 fim

--- aula3/matchcase.py
+++ aula3/matchcase.py
@@ -1,16 +1,4 @@
-
-
 time = input("Digite seu time: ")
-
-# if time == 'Corinthians':
-#     print("Você é um Timão!")
-# elif time == "Bahia":
-#     print("Você é do Esquadrão")
-# elif time == "Gremio":
-#     print("Você é do Imortal")
-# else:
-#     print("Você não é um Timão!")
-
 
 match time:
     case "Corinthians":
@@ -21,5 +9,4 @@ match time:
         print("Você é imortal!")
     case _:
         print("Quem não é não se mete")
-
 
